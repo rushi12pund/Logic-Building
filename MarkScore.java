0fef6fafc0069e5ae4d63c774f5591c82511d235
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-08-07 16:38:57 +0530

    Create MarkScore.java

---
+++ MarkScore.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class Marksscore {
+
+	public static void main(String[] args) {
+	
+	
+		Scanner sc =new Scanner(System.in);
+	
+		//scanner not importing 
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
