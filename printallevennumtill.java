ca11b1ddac6e814f9d43888dffb2a53c0e41c7f0
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-08-07 16:37:55 +0530

    Create printallevennumtill.java

---
+++ printallevennumtill.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class printallevennumbetill {
+
+	public static void main(String[] args) {
+		
+		int n=50;
+		
+		for(int i=0;i<n;i++) {
+			
+			if(i%2==0) {
+				System.out.println(i);
+			
+			
+			
+			}
+			
+		}
+		
+		
+	}
+
+}
