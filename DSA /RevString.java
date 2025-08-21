96e4a9bb0b84c9f63a43a5c50c5ec8d5168be430
Rushikesh Pund <154165719+rushi12pund@users.noreply.github.com>
2025-08-21 22:00:34 +0530

    Create RevString.java

---
+++ DSA /RevString.java
@@ -0,0 +1,20 @@
+package com.example.demo.string;
+
+public class RevString3 {
+
+	public static void main(String[] args) {
+		
+          String Input = "Hello" ;
+          
+          for(int i=Input.length()-1;i>=0;i--) {
+        	  
+        	  char s = Input.charAt(i);
+        	  
+        	  System.out.println(s);
+          }
+		
+          
+		
+	}
+
+}
